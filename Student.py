e46aebaf7623cf894e7e730324c2053e5c535173
Harshp-hp <72340908+Harshp-hp@users.noreply.github.com>
2020-10-21 23:04:38 +0530

    Update Student.py

--- Student.py
+++ Student.py
@@ -15,6 +15,7 @@ def write_into_csv(info_list):
         
 if __name__ == '__main__':
     condition = True
+    student_num = 1
 
     while(condition):
         Student_in = input("Enter some Student info as follows [name.age,city,state,contact_number,emial_id,other]")
@@ -34,6 +35,7 @@ if __name__ == '__main__':
             condition_check = input("Enter (yes/no) if you want to enter information for the another student:")
             if condition_check == 'yes':
                 condition = True
+                student_num = student_num + 1
             elif condition_check == 'no':
                 condition = False
         elif choice_Check == "no":
